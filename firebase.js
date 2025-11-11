57bfd4b7b75d3ba9ddd579e2c39d3cf7dd3d599b
Habib-YT <mshabib471@gmail.com>
2025-11-11 22:47:53 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
-// Firebase 12.5.0 CDN version
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDocs, collection, addDoc, query, where } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAaLdtzOIZVYB-Bdc42CXm2T8iclWLc4o0",
@@ -13,8 +13,7 @@ const firebaseConfig = {
   measurementId: "G-GNY1T88D34"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
