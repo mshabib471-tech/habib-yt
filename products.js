4f1d26241ffa84ed390bfed16862e48d49de2a4b
Habib-YT <mshabib471@gmail.com>
2025-12-29 21:07:10 +0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,66 @@
+const products = [
+  {
+    id: 1,
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirt",
+    price: 78,
+    image: "./image/Product-img/f1.jpg",
+    rating: 5
+  },
+  {
+    id: 2,
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirt",
+    price: 78,
+    image: "./image/Product-img/f2.jpg",
+    rating: 5
+  },
+  {
+    id: 3,
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirt",
+    price: 78,
+    image: "./image/Product-img/f3.jpg",
+    rating: 5
+  },
+  {
+    id: 4,
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirt",
+    price: 78,
+    image: "./image/Product-img/f4.jpg",
+    rating: 5
+  },
+  {
+    id: 5,
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirt",
+    price: 78,
+    image: "./image/Product-img/f5.jpg",
+    rating: 5
+  },
+  {
+    id: 6,
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirt",
+    price: 78,
+    image: "./image/Product-img/f6.jpg",
+    rating: 5
+  },
+  {
+    id: 7,
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirt",
+    price: 78,
+    image: "./image/Product-img/f7.jpg",
+    rating: 5
+  },
+  {
+    id: 8,
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirt",
+    price: 78,
+    image: "./image/Product-img/f8.jpg",
+    rating: 5
+  }
+];
